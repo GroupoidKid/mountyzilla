edfc161466ba2f2bd71f59dfb8b7b1428aaefe35
Dabihul (79738) <tontondabi@sfr.fr>
2014-06-23 23:18:07 +0200

    Relecture du néo-equip_FF

--- scripts_online/equip_FF.js
+++ scripts_online/equip_FF.js
@@ -29,12 +29,12 @@
 function traiteChampis() {
 	try{
 		var tr = document.getElementById('mh_objet_hidden_Champignon');
-		var trlist = document.evaluate('./td/table/tbody/tr', tr, null, 7, null);
-		}
-	catch(e){return;}
-	if(trlist.length<=0) return;
-	for(var i=0 ; i<trlist.snapshotLength ; i++) {
-		var node = trlist.snapshotItem(i).childNodes[7];
+		var trList = document.evaluate('./td/table/tbody/tr', tr, null, 7, null);
+	}
+	catch(e){ return; }
+	if(trList.length<=0) { return; }
+	for(var i=0 ; i<trList.snapshotLength ; i++) {
+		var node = trList.snapshotItem(i).childNodes[7];
 		var str = node.textContent.trim();
 		var type = str.slice(0,str.lastIndexOf(' '));
 		var mundi = mundiChampi[type];
@@ -44,76 +44,76 @@ function traiteChampis() {
 		var img = createAltImage(urlImg,'EM','Mundidey '+mundi);
 		appendText(node,' ');
 		node.appendChild(img);
-		}
 	}
+}
 
 function traiteCompos() {
 	try {
 		var tr = document.getElementById('mh_objet_hidden_Composant');
 		var tbody = document.evaluate("./td/table/tbody",
 			tr, null, 9, null).singleNodeValue;
-		}
-	catch(e) {return;}
-	insererInfosEM(tbody);
 	}
+	catch(e){ return; }
+	insererInfosEM(tbody);
+}
 
 function traiteMinerai() {
 	try{
 		var tr = document.getElementById('mh_objet_hidden_Minerai');
-		var trlist = document.evaluate('./td/table/tbody/tr', tr, null, 7, null);
-		}
-	catch(e){return;}
-	if(trlist.length<=0) return;
+		var trList = document.evaluate('./td/table/tbody/tr', tr, null, 7, null);
+	}
+	catch(e){ return; }
+	if(trList.length<=0) { return; }
 	var totaux = {};
 	var str;
-	for(var i=0 ; i<trlist.snapshotLength ; i++) {
-		var node = trlist.snapshotItem(i);
+	for(var i=0 ; i<trList.snapshotLength ; i++) {
+		var node = trList.snapshotItem(i);
 		var nature = node.childNodes[7].textContent,
 			caracs = node.childNodes[9].textContent;
 		var taille = Number(caracs.match(/\d+/));
 		var coef = 1;
-		if(caracs.indexOf('Moyen')!=-1) coef = 2;
-		else if(caracs.indexOf('Normale')!=-1) coef = 3;
-		else if(caracs.indexOf('Bonne')!=-1) coef = 4;
-		else if(caracs.indexOf('Exceptionnelle')!=-1) coef = 5;
+		if(caracs.indexOf('Moyen')!=-1) { coef = 2; }
+		else if(caracs.indexOf('Normale')!=-1) { coef = 3; }
+		else if(caracs.indexOf('Bonne')!=-1) { coef = 4; }
+		else if(caracs.indexOf('Exceptionnelle')!=-1) { coef = 5; }
 		if(nature.indexOf('Mithril')!=-1) {
 			coef = 0.2*coef;
 			str = ' | UM: ';
-			}
+		}
 		else {
 			coef = 0.75*coef+1.25;
 			if(nature.indexOf('Taill')!=-1) coef = 1.15*coef;
 			str = ' | Carats: ';
-			}
-		var carats = Math.round(taille*coef)
+		}
+		var carats = Math.round(taille*coef);
 		appendText(node.childNodes[9], str+carats );
 		if(!totaux[nature]) {
 			totaux[nature] = [taille,carats];
-			}
+		}
 		else {
 			totaux[nature][0] += taille;
 			totaux[nature][1] += carats;
-			}
 		}
+	}
 	str = 'Total : ';
 	for(var nature in totaux) {
 		if(str.length>8) str += ', ';
 		if(nature.indexOf('Mithril')!=-1) {
 			str += nature+totaux[nature][1]+' UM';
-			}
+		}
 		else {
 			str += nature+totaux[nature][0]+'U/'
 				+totaux[nature][1]+'c';
-			}
 		}
+	}
 	/*var node = document.getElementById('mh_plus_Minerai');
 	var titre = document.evaluate("./td[contains(./b/text(),'Minerai')]",
 		node.parentNode.parentNode.parentNode, null, 9, null).singleNodeValue;
 	if(!titre) return;*/
 	// Il faut préalablement injecter du CSS pour ne pas hériter de 'mh_titre3'
-	var td = appendTdText(trlist.snapshotItem(0).parentNode, '('+str+')');
+	var td = appendTdText(trList.snapshotItem(0).parentNode, '('+str+')');
 	td.colSpan = 7;
-	}
+}
 
 
 start_script();
